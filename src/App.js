f84754d07769bb1061f0af868cdc3ee7df9eedb6
developer-munir <devolopermunir@gmail.com>
2022-12-18 19:52:18 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import { Container } from "@mui/material";
 import "./App.css";
 import Banner from "./Home/Banner/Banner";
 import Offer from "./Home/Offer/Offer/Offer";
+import Navbar from "./shared/Navbar/Navbar";
 
 function App() {
   return (
     <Container className="offer-container" maxWidth="xl">
+      <Navbar></Navbar>
       <Banner></Banner>
       <Offer></Offer>
     </Container>
