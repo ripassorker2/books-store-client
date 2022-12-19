5f2cbe0f9bbb092b44d8871720759006739ebc8e
unknown <ojitdebnathfreelancer.com>
2022-12-19 19:38:57 +0600

    add tow folder

---
+++ src/Home/HotSell/HotSell.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HotSell = () => {
+    return (
+        <div>
+            <h1>This is hot sell Rifat</h1>
+        </div>
+    );
+};
+
+export default HotSell;
