d10763d80814618227c1f2f8344f712135043ce4
Ripassorker <ripassorker02@glail.com>
2023-01-01 11:23:09 +0600

    aded product , show all user , all buyar

--- src/Pages/Dashboard/Buyer/Buyer.jsx
+++ src/Pages/Dashboard/Buyer/Buyer.jsx
@@ -1,8 +1,39 @@
-import { DeleteForeverOutlined } from "@mui/icons-material";
 import { Avatar } from "@mui/material";
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
+import { toast } from "react-hot-toast";
+import { MdDelete } from "react-icons/md";
+import Loader from "../../../utility/Loader/Loader";
 import "../Dashboard.css";
 const Buyer = () => {
+  const {
+    data: buyers = [],
+    refetch,
+    isLoading,
+  } = useQuery({
+    queryKey: ["buyers"],
+    queryFn: () =>
+      fetch(`http://localhost:5000/buyer`).then((res) => res.json()),
+  });
+  const handleDelete = (id) => {
+    const aggre = window.confirm("Are sure ?You want to remove this?");
+    if (aggre) {
+      fetch(`http://localhost:5000/buyer/${id}`, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.deletedCount) {
+            toast.success("Succesfully delete this buyer!!");
+            refetch();
+          }
+        })
+        .catch((err) => console.error(err));
+    }
+  };
+  if (isLoading) {
+    return <Loader />;
+  }
   return (
     <div>
       <h1 className="text-3xl font-semibold mb-4">Buyer</h1>
@@ -14,40 +45,35 @@ const Buyer = () => {
           <p className="border p-3">Delete</p>
         </div>
         {/* data */}
-        <div className=" container-table-data  ">
-          <div className="border p-3">
-            <Avatar
-              alt="Remy Sharp"
-              src="/static/images/avatar/1.jpg"
-              className="avatar-user"
-            />
-          </div>
-          <div className="border p-3 ">
-            <p>Munir Hossain Juwel</p>
-            <span className="text-sm text-green-800 font-semibold">Buyer</span>
-          </div>
-          <div className="border p-3 ">munirhossainjuwel@gmail.com</div>
-          <div className="border p-3 ">
-            <DeleteForeverOutlined></DeleteForeverOutlined>
-          </div>
-        </div>
-        <div className=" container-table-data  ">
-          <div className="border p-3">
-            <Avatar
-              alt="Remy Sharp"
-              src="/static/images/avatar/1.jpg"
-              className="avatar-user"
-            />
-          </div>
-          <div className="border p-3 ">
-            <p>Munir Hossain Juwel</p>
-            <span className="text-sm text-green-800 font-semibold">Buyer</span>
-          </div>
-          <div className="border p-3 ">munirhossainjuwel@gmail.com</div>
-          <div className="border p-3 ">
-            <DeleteForeverOutlined></DeleteForeverOutlined>
-          </div>
-        </div>
+
+        {buyers?.map((buyer) => (
+          <>
+            <div className=" container-table-data  ">
+              <div className="border p-3">
+                <Avatar
+                  alt="Remy Sharp"
+                  src="/static/images/avatar/1.jpg"
+                  className="avatar-user"
+                />
+              </div>
+              <div className="border p-3 ">
+                <p>{buyer?.name}</p>
+                <span className="text-sm text-green-800 font-semibold">
+                  {buyer.role}
+                </span>
+              </div>
+              <div className="border p-3 ">{buyer?.email}</div>
+              <div className="border p-3 ">
+                <button
+                  onClick={() => handleDelete(buyer?._id)}
+                  className="bg-[#fc3333]  text-white sm:w-10 sm:h-10 h-6 w-6 rounded-full flex justify-center items-center"
+                >
+                  <MdDelete className="h-8 w-8" />
+                </button>
+              </div>
+            </div>
+          </>
+        ))}
       </div>
     </div>
   );
--- src/Pages/Dashboard/MyProducts/MyProducts.js
+++ src/Pages/Dashboard/MyProducts/MyProducts.js
@@ -1,14 +1,36 @@
-import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import React, { useContext } from "react";
+import { toast } from "react-hot-toast";
+import { AuthContext } from "../../../Context/AuthProvider/AuthProvider";
+import Loader from "../../../utility/Loader/Loader";
 import ProductCard from "./ProductCard";
 
 const MyProducts = () => {
+  const { user } = useContext(AuthContext);
+
+  const {
+    data: products = [],
+    refetch,
+    isLoading,
+  } = useQuery({
+    queryKey: ["products"],
+    queryFn: () =>
+      fetch(`http://localhost:5000/product/${user?.email}`).then((res) =>
+        res.json()
+      ),
+  });
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
   return (
     <div>
       <h1 className="text-3xl font-semibold mb-3">My Products</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        <ProductCard></ProductCard>
-        <ProductCard></ProductCard>
-        <ProductCard></ProductCard>
+        {products?.map((product) => (
+          <ProductCard key={product._id} product={product} refetch={refetch} />
+        ))}
       </div>
     </div>
   );
