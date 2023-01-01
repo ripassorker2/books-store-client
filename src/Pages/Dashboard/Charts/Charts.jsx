dfbc27607ba1a77250b258030e959cd394256d1b
developer-munir <devolopermunir@gmail.com>
2023-01-01 18:00:27 +0600

    change chart data

--- src/Pages/Dashboard/Charts/Charts.jsx
+++ src/Pages/Dashboard/Charts/Charts.jsx
@@ -1,29 +1,11 @@
-import React, { PureComponent } from "react";
-import { ResponsiveContainer, PieChart, Pie, Legend } from "recharts";
+import React from "react";
 
-const data = [
-  { name: "Group A", value: 400 },
-  { name: "Group B", value: 300 },
-  { name: "Group C", value: 300 },
-  { name: "Group D", value: 200 },
-];
+const Charts = () => {
+  return (
+    <div>
+      <h1 className="font-semibold text-5xl text-center mt-4">Welcome</h1>
+    </div>
+  );
+};
 
-export default class Example extends PureComponent {
-  // static demoUrl =
-  //   "https://codesandbox.io/s/pie-chart-in-responsive-container-qyv6t";
-
-  render() {
-    return (
-      <div>
-        {/* <div style={{ width: "100%", height: 300, overflow: "hidden" }}>
-          <ResponsiveContainer>
-            <PieChart>
-              <Pie dataKey="value" data={data} fill="#8884d8" label />
-            </PieChart>
-          </ResponsiveContainer>
-        </div> */}
-        <h1 className="font-semibold text-5xl text-center mt-4">Welcome</h1>
-      </div>
-    );
-  }
-}
+export default Charts;
